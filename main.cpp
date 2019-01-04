81dff5b80bf852f52983a074838391dfa1adc3e3
BambangPriam <45525619+BambangPriam@users.noreply.github.com>
2019-01-04 22:17:48 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   float S,v,t;
+    cout <<"Menghitung jarak tempuh"<<endl;
+    cout <<"Masukkan kecepatan (km/jam) = ";
+    cin >> v;
+    cout<<"Masukkan waktu tempuh (detik) = ";
+    cin>> t;
+
+    S = v*t;
+    cout<< "Jarak tempuh adalah : " <<S/3600; // dibagi 3600 karena konversi dari jam ke detik
+
+    return 0;
+}
